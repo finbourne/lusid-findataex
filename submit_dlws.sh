294fb55022de3c7d1eb6371be62a4e35776e1a69
Khalid Keenan <khalid.keenan@finbourne.com>
2020-11-23 15:28:05 +0000

    SE-511 Updating BBGDLWS Submit script
    - Adding changes required for testing DLWS submission

--- submit_dlws.sh
+++ submit_dlws.sh
@@ -67,12 +67,20 @@ if [[ ! -s $certificate_file ]] ; then
 	exit 1
 fi
 
-echo $certificate_file output is...
-cat $certificate_file
+
+
+echo "pre base encode"
 ls -ltr $certificate_file
 
+# decode to cert
+pk12_cert_file=$(mktemp)
+base64 -d $certificate_file > $pk12_cert_file
+
+echo "post base encode"
+ls -ltr $pk12_cert_file
+
 # set certificate file path as env var for use by findataex
-export BBG_DL_CERT=$certificate_file
+export BBG_DL_CERT=$pk12_cert_file
 
 
 # if no filesystem provided set to local
@@ -80,21 +88,66 @@ if [[ -z $filesystem ]] ; then
   filesystem=Lusid
 fi
 
-if [[ $action = "getdata" ]] && [[ ! -z $portfolios ]]
+echo "---testing sample args ----"
+echo $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+echo $LUSID_FINDATA_EX_DLL getdata -p $portfolios -t $intsrument_id_type -o $output_dir -f $filesystem -d $data_fields
+echo "---end testing sample args ----"
+
+echo $action$action
+if [[ "$action" == " getdata" ]]
 then
-  dotnet $LUSID_FINDATA_EX_DLL getdata -p $portfolios -o ${0} -f $filesystem -d ${d}
-elif [[ $action = "getdata" ]] &&  [[ ! -z $intsrument_id_type ]]
-then
-  dotnet $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
-elif [[ $action = "getaction" ]] &&  [[ ! -z $portfolios ]]
-then
-  dotnet $LUSID_FINDATA_EX_DLL getaction -p $portfolios -o $output_dir  -f $filesystem -c $corp_actions
-elif [[ $action = "getaction" ]] &&  [[ ! -z $intsrument_id_type ]]
-then
-  dotnet $LUSID_FINDATA_EX_DLL getaction -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -c $corp_actions
+  echo "get data properly detected"
 fi
+
+if [[ ! -z "$intsrument_id_type" ]]
+then
+  echo "instrument ids are NOT empty"
+fi
+
+#if [[ "$action" == " getdata" ]] && [[ ! -z "$portfolios" ]]
+#then
+#  echo $LUSID_FINDATA_EX_DLL getdata -p $portfolios -o ${0} -f $filesystem -d ${d}
+#  dotnet $LUSID_FINDATA_EX_DLL getdata -p $portfolios -o ${0} -f $filesystem -d ${d}
+#elif [[ "$action" == " getdata" ]] &&  [[ ! -z "$intsrument_id_type" ]]
+#then
+#  echo $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+# dotnet $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+#elif [[ "$action" == "getaction" ]] &&  [[ ! -z "$portfolios" ]]
+#then
+#  echo $LUSID_FINDATA_EX_DLL getaction -p $portfolios -o $output_dir  -f $filesystem -c $corp_actions
+#  dotnet $LUSID_FINDATA_EX_DLL getaction -p $portfolios -o $output_dir  -f $filesystem -c $corp_actions
+#elif [[ "$action" == "getaction" ]] &&  [[ ! -z "$intsrument_id_type" ]]
+#then
+#  echo $LUSID_FINDATA_EX_DLL getaction -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -c $corp_actions
+#  dotnet $LUSID_FINDATA_EX_DLL getaction -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -c $corp_actions
+if [[ ! -z "$portfolios" ]]
+then
+  echo echo $LUSID_FINDATA_EX_DLL getdata -p $portfolios -t $intsrument_id_type -o $output_dir -f $filesystem -d $data_fields
+  dotnet $LUSID_FINDATA_EX_DLL getdata -p $portfolios -t $intsrument_id_type -o $output_dir -f $filesystem -d $data_fields
+elif [[ ! -z "$intsrument_id_type" ]]
+then
+  echo $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+  dotnet $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+else 
+  echo missing minimum required args for findataex calls...
+  echo findataexDll:$LUSID_FINDATA_EX_DLL
+  echo action:$action
+  echo portfolios:$portfolios
+  echo intsrument_id_type :$intsrument_id_type
+  echo instrument_ids:$instrument_ids
+  echo filesystem:$filesystem
+  echo output_dir:$output_dir
+  echo data_fields:$data_fields
+  echo corp_actions:$corp_actions
+fi
+
+#echo running default findataex anyway...
+#dotnet $LUSID_FINDATA_EX_DLL getdata -t $intsrument_id_type -i $instrument_ids -o $output_dir  -f $filesystem -d $data_fields
+#dotnet $LUSID_FINDATA_EX_DLL getdata -p $portfolios -o ${0} -f $filesystem -d ${d}
+
+echo findataex command completed with code $?
 
 # delete the tmp file for the identity file
 trap "rm -f $tmp_file" EXIT
-
+trap "rm -f $pk12_cert_file" EXIT
 exit 0
